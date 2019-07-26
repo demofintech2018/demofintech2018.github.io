d1f05feb4d0900c57e6c641b3784020c079bd5f5
demofintech2018 <44388627+demofintech2018@users.noreply.github.com>
2019-07-26 18:36:10 +0800

    test

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ function startWebRTC(isOfferer) {
 body.setAttribute("style","position:;");
                 remoteVideo.setAttribute("style", "width: 140%;height: auto;position: absolute;top: 0;left: 0;");
                 localVideo.setAttribute("style", "display: ;");
-                canvasOutputLocal.setAttribute("style", "display:;position:absolute;top:610;left:918;width:257;height:193");
+                canvasOutputLocal.setAttribute("style", "display:;position:absolute;top:610;left:918;width:300;height:200");
 
                 var canvasLocal = document.getElementById('canvasOutputLocal');
                 var ctxLocal = canvasLocal.getContext('2d');
